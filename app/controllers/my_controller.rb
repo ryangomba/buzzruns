bd39e74ffde4e666e8e1eb5a94bf2b550f1832c1
Ryan Gomba <Ryan@lawn-128-61-17-231.lawn.gatech.edu>
2011-10-04 16:22:55 -0400

    Navbar

---
+++ app/controllers/my_controller.rb
@@ -0,0 +1,15 @@
+class MyController < ApplicationController
+
+    def index
+        redirect_to :action => 'journal'
+    end
+
+    def journal
+        # bla
+    end
+
+    def performances
+        # bla
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Buzzruns::Application.routes.draw do
-    get "home/index"
-
-    # The priority is based upon order of creation:
-    # first created -> highest priority.
-
+ 
     # Sample of regular route:
     #   match 'products/:id' => 'catalog#view'
     # Keep in mind you can assign values other than :controller and :action
@@ -48,13 +44,12 @@ Buzzruns::Application.routes.draw do
     #     resources :products
     #   end
 
-    # You can have the root of your site routed with "root"
-    # just remember to delete public/index.html.
+    # See how all your routes lay out with "rake routes"
+    
     root :to => 'home#index'
 
-    # See how all your routes lay out with "rake routes"
+    match 'my' => 'my#index'
+    match 'my/journal' => 'my#journal'
+    match 'my/performances' => 'my#performances'
 
-    # This is a legacy wild controller route that's not recommended for RESTful applications.
-    # Note: This route will make all actions in every controller accessible via GET requests.
-    # match ':controller(/:action(/:id(.:format)))'
 end
