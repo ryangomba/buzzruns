143683843aa2c0de5fe75767288aca5d99d5106f
Ryan Gomba <Ryan@lawn-128-61-16-92.lawn.gatech.edu>
2011-10-04 20:30:45 -0400

    Some basic athlete functionality (list, delete)

--- app/helpers/athletes_helper.rb
+++ app/helpers/athletes_helper.rb
@@ -1,2 +1,9 @@
 module AthletesHelper
+
+    def split_sexes(athletes)
+        men = athletes.find_all{|a| a.sex == 'male' }
+        women = athletes.find_all{|a| a.sex == 'female' }
+        return { :Men => men, :Women => women }
+    end
+
 end
